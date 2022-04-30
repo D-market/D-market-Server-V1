6633bd3a7081a43d745dd7a0f0eb51e7a62b1c13
우준성 <woojs0505@naver.com>
2022-05-01 00:31:42 +0900

    :tada: :: start project

---
+++ src/main/java/com/b1nd/dmarket/DmarketApplication.java
@@ -0,0 +1,13 @@
+package com.b1nd.dmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/b1nd/dmarket/DmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b1nd.dmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
